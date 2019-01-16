59a92789165c81dfa1006065af6d00f5c85d6595
PETIT Baptiste <baptiste.petit.96@gmail.com>
2019-01-16 10:56:56 +0100

    Création du fichier contenant les fonctions et quelques fonctions nommées mais vide, pour préparer les appels

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+public function getContentJson($filePath){
+	# code...
+}
+public function setNewContentJson($name, $series){
+	# code...
+}
+public function addNewSerie($serie){
+	# code...
+}
+public function noteSerie($serie, $note, $avis = false){
+	# code...
+}
+public function displayListSeries(){
+	# code...
+}
+public function displayFav(){
+
+}
+?>
